192dd6eb8a3621e6a158a589ca42ed2b597c3e3b
user0105722 <jacobsecureacc@gmail.com>
2025-06-03 16:16:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+body {
+  font-family: sans-serif;
+  background: #111;
+  color: #fff;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.chat-container {
+  flex: 1;
+  padding: 10px;
+  overflow-y: scroll;
+}
+#chat-form {
+  display: flex;
+  padding: 10px;
+  background: #222;
+}
+#user-input {
+  flex: 1;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+}
+button {
+  margin-left: 10px;
+  padding: 10px;
+  background: #00aaff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+}
